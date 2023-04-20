d1aaef061a3b5b545d50f03afa11770c2d7d769f
Swapnil Patil <swapnil@Swapnils-MacBook-Pro.local>
2023-04-20 16:27:23 +0530

    work on hostel student

---
+++ src/app/interface/hostel-room.ts
@@ -0,0 +1,6 @@
+export interface HostelRoom {
+	id?: number;
+	name: string;
+	no_of_students: number;
+	hostel_id: number;
+}
---
+++ src/app/interface/hostel.ts
@@ -0,0 +1,8 @@
+export interface Hostel {
+	id?: number;
+	name: string;
+	rooms: number;
+	rector: string;
+	contact_number: number;
+	rate: number;
+}
