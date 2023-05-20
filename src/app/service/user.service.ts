1ba9694fabd189d8119a0a9daceb3a2eed9f1f3d
Swapnil Patil <swapnil@Swapnils-MacBook-Pro.local>
2023-05-20 22:26:33 +0530

    work on logout

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginService } from './../service/login.service'
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './../interface/user'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  URL = "http://localhost:8080"
+  //URL = "http://54.237.98.76:8080"
+
+  constructor(private http: HttpClient, private loginService: LoginService) { }
+
+  getUsers(): Observable<User[]> {
+    const myHeaders = this.loginService.getHeaders();
+    return this.http.get<User[]>(`${this.loginService.URL}/users`, {headers: myHeaders});
+  }
+
+}
---
+++ src/app/users/users/users.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './../../service/user.service';
+import { LoginService } from './../../service/login.service';
+import { User } from './../../interface/user';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent  implements OnInit {
+
+  public users: User[] = [];
+  constructor(private userService: UserService, private loginService: LoginService){}
+
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  errorHandle(error: any): void {
+    if(error.status == 401) {
+      this.loginService.toLogin();
+    }
+  }
+
+  loadUsers(): void {
+    this.userService.getUsers().subscribe (
+      (response: any) => this.assignUsers(response),
+      (error: any) => this.errorHandle(error),
+      () => console.log('Done getting Users......')
+    );
+  }
+  
+  assignUsers(response: any) {
+    this.users = response
+  }
+}
