6628fbaee9106890ddb6b4c0b48ff20d246af2f7
Swapnil Patil <swapnil@Swapnils-MacBook-Pro.local>
2025-04-23 12:30:13 +0530

    work on loading image

---
+++ src/app/graphs/loading-image/loading-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-image',
+  templateUrl: './loading-image.component.html',
+  styleUrls: ['./loading-image.component.css']
+})
+export class LoadingImageComponent {
+
+}
